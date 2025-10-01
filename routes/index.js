9190ffa6f847153b985d3c2bdd8d4c49d4957d7b
Ankit Sharma <ritiksharma3k2@gmail.com>
2025-10-01 18:08:33 +0530

    Basic Routing done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
-
+const userModel = require("./users")
+const postModel = require("./posts")
+const localStrategy  = require("passport-local");
+const passport = require('passport');
+passport.authenticate(new localStrategy(userModel.authenticate()))
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/profile',isLoggedin, function(req, res, next) {
+  res.send(profile);
+});
+
+router.post("/register",function(req,res,next){
+  const {username,fullname,email}= req.body
+  let newUser = new userModel({username,fullname,email})
+  userModel.register(newUser,req.body.password)
+  .then(function(){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/profile")
+    })
+  })
+})
+
+router.post("/login",passport.authenticate("local",{
+  successredirect : "/profile",
+  failureredirect : "/"
+}),function(req,res,next){
+
+})
+
+router.post('/logout', function(req, res, next){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+function isLoggedin(req,res,next){
+  if(req.isAuthenticated()) return next()
+  res.redirect("/")
+}
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
+const Post = require('./posts');
+const plm = require("passport-local-mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/pintrest")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  password: {
+    type: String,
+    required: true,
+    
+  },
+  post: [{
+    type: mongoose.Types.ObjectId,
+    ref: "Post",
+    default: []}]
+  ,
+  dp: {
+    type: String, // URL or path to display picture
+    default: ''
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/.+\@.+\..+/, 'Please fill a valid email address']
+  },
+  fullname: {
+    type: String,
+    required: true,
+    trim: true
+  }
+}, {
+  timestamps: true // adds createdAt and updatedAt fields
 });
 
-module.exports = router;
+// Export the model
+userSchema.plugin(plm)
+ module.exports = mongoose.model('User', userSchema);
+
